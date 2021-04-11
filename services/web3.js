dcab9a838d1a97bd023f0a74e84bcc7aa81b2d1a
Teerapat Chintanakit <t.chintanakit@gmail.com>
2021-04-12 06:35:53 +0700

    First Commit

---
+++ services/web3.js
@@ -0,0 +1,6 @@
+import Web3 from 'web3'
+
+const web3 = new Web3('https://bsc-dataseed1.binance.org:443')
+// const web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545')
+
+export default web3
